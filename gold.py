1b7eabec5da273bee5e1a3cafb53dc7eb92fa886
st22112 <st22112@ormiston.school.nz>
2022-08-11 13:39:44 +1200

    a

--- gold.py
+++ gold.py
@@ -1,4 +1,9 @@
 # st22112
 # 2022-08-09
 
-total_gold= 107
+total_gold = 107
+individual_gold = int(total_gold/4)
+individual_gold_remainder = int(total_gold%4)
+print(total_gold, "gold coin all together")
+print(individual_gold, "gold coins to be divided evenly with 4 pirates")
+print(individual_gold_remainder, "coins left over")

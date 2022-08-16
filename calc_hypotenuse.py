7570e3b3d708a553f1ab45b02b6230888c05b364
st22112 <st22112@ormiston.school.nz>
2022-08-16 14:02:56 +1200

    change date

--- calc_hypotenuse.py
+++ calc_hypotenuse.py
@@ -1,5 +1,5 @@
 #st22112
-#2022-08-15
+#2022-08-16
 from math import sqrt
 print("I will work out the length of a hypotenuse for you")
 print ("The length of the hypotenuse is {:0.2f}".format( sqrt(float(input("What is the length of the first side, a? "))**2 + float(input("What is the length of the second side, b? "))**2) ))

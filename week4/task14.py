5b8edc9338a011c53142d815610206d57535d5b1
st22112 <st22112@ormiston.school.nz>
2022-09-05 09:48:05 +1200

    af a

---
+++ week4/task14.py
@@ -0,0 +1,8 @@
+# st22112
+# 2022-09-05
+# I have $66.00 to buy lunch for 12 people. How much can each person spend? $66.00  for 12 people
+
+import math
+balance = float(6600) # cents
+people = float(12)
+print("${}".format((balance//12)/100))

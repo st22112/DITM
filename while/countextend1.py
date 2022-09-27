363bf1dcdfe521628f93057376ff17c91eb30383
st22112 <st22112@ormiston.school.nz>
2022-09-27 14:50:07 +1300

    saijhf

---
+++ while/countextend1.py
@@ -0,0 +1,5 @@
+i = 0
+upto = int(input("Please input an integer to count up to: "))
+while i <= upto:
+    print(i)
+    i = i+2
---
+++ while/countextend2.py
@@ -0,0 +1,6 @@
+i = 0
+up = int(input("Please input an integer to count in: "))
+upto = int(input("Please input an integer to count up to: "))
+while i <= upto:
+    print(i)
+    i = i+up

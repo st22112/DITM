80f4477df573b3e2706c9e2f7cb37f07341e6790
st22112 <st22112@ormiston.school.nz>
2022-10-27 16:48:34 +1300

    holy shit

---
+++ tuples/8.py
@@ -0,0 +1,19 @@
+tuple1 = (('a', 23),('b', 37),('c', 11), ('d',29))
+sort = []
+
+print(tuple1)
+
+for i in range(len(tuple1)):
+    if len(sort) == 0:
+        sort.append(tuple1[i])
+    else:
+        for j in range(len(sort)):
+            if sort[j][1] > tuple1[i][1]:
+                sort.insert(j, tuple1[i])
+                break
+            if j == len(sort)-1 and sort[j][1] < tuple1[i][1]:
+                sort.append(tuple1[i])
+                break
+
+tuple1 = tuple(sort)
+print(tuple1)

39d15c7e5558aec7616e6083c987a31bed05d2e5
st22112 <st22112@ormiston.school.nz>
2022-10-27 21:18:32 +1300

    drugs

--- tuples/8s.py
+++ tuples/8s.py
@@ -1,13 +1,13 @@
 # teamwork with shun, yay
-tuple1 = (('a', 23),('b', 37),('c', 11), ('d',29))
+tuple1 = (('a', 23), ('b', 37), ('c', 11), ('d', 29), ('duplicate test', 23))
 list1 = []
 
 for i in tuple1:
     a = 0
     for j in tuple1:
-        if i[1]<j[1]: # change < to > to reverse
+        if i[1]>j[1]: # change < to > to reverse
             a = a+1
-    list1.append(tuple1[a])
+    list1.insert(a, i)
 
 tuple1 = tuple(list1)
 print(tuple1)

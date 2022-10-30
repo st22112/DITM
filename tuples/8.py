cfbb42697be8621c1c49983333fdbd6bbef57bf4
st22112 <st22112@ormiston.school.nz>
2022-10-31 09:36:30 +1300

    a

--- tuples/8.py
+++ tuples/8.py
@@ -4,15 +4,15 @@ sort = []
 print(tuple1)
 
 for i in range(len(tuple1)):
-    if len(sort) == 0:
-        sort.append(tuple1[i])
-    else:
-        for j in range(len(sort)):
-            if sort[j][1] > tuple1[i][1]:
-                sort.insert(j, tuple1[i])
-                break
-            if j == len(sort)-1:
-                sort.append(tuple1[i])
+	if len(sort) == 0:
+		sort.append(tuple1[i])
+	else:
+		for j in range(len(sort)):
+			if sort[j][1] > tuple1[i][1]:
+				sort.insert(j, tuple1[i])
+				break
+			if j == len(sort)-1:
+				sort.append(tuple1[i])
 
 tuple1 = tuple(sort)
 print(tuple1)
--- tuples/8s.py
+++ tuples/8s.py
@@ -1,13 +1,14 @@
 # teamwork with shun, yay
 tuple1 = (('a', 23), ('b', 37), ('c', 11), ('d', 29), ('duplicate test', 23))
 list1 = []
+print(tuple1)
 
 for i in tuple1:
-    a = 0
-    for j in tuple1:
-        if i[1]>j[1]: # change < to > to reverse
-            a = a+1
-    list1.insert(a, i)
+	a = 0
+	for j in list1:
+		if i[1]>=j[1]: # change < to > to reverse
+			a += 1
+	list1.insert(a, i)
 
 tuple1 = tuple(list1)
 print(tuple1)

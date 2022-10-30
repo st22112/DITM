3b71b99e20051a0bf024f1ee0c10c4ae78153d8a
st22112 <st22112@ormiston.school.nz>
2022-10-31 09:50:58 +1300

    a

--- tuples/8.py
+++ tuples/8.py
@@ -1,18 +1,14 @@
-tuple1 = (('a', 23),('b', 37),('c', 11), ('d',29))
-sort = []
-
+# teamwork with shun, yay
+tuple1 = (('a', 23), ('b', 37), ('c', 11), ('d', 29))
+list1 = []
 print(tuple1)
 
-for i in range(len(tuple1)):
-	if len(sort) == 0:
-		sort.append(tuple1[i])
-	else:
-		for j in range(len(sort)):
-			if sort[j][1] > tuple1[i][1]:
-				sort.insert(j, tuple1[i])
-				break
-			if j == len(sort)-1:
-				sort.append(tuple1[i])
+for i in tuple1:
+	a = 0
+	for j in list1:
+		if i[1]>=j[1]: # change < to > to reverse
+			a += 1
+	list1.insert(a, i)
 
-tuple1 = tuple(sort)
+tuple1 = tuple(list1)
 print(tuple1)
--- tuples/8s.py
+++ tuples/8s.py
@@ -1,14 +1,15 @@
-# teamwork with shun, yay
-tuple1 = (('a', 23), ('b', 37), ('c', 11), ('d', 29), ('duplicate test', 23))
+tuple1 = (('a', 23),('b', 37),('c', 11), ('d',29))
 list1 = []
-print(tuple1)
-
-for i in tuple1:
-	a = 0
-	for j in list1:
-		if i[1]>=j[1]: # change < to > to reverse
-			a += 1
-	list1.insert(a, i)
-
-tuple1 = tuple(list1)
-print(tuple1)
+list2 = []
+for i,j in tuple1:
+	a=0
+	for k,l in tuple1:
+		if j>l:
+			a = a+1
+	list1.append(a)
+b=0
+for x in tuple1:
+	list2.insert(list1[b],x)
+	b=b+1
+tuple1 = tuple(list2)
+print (tuple1)
